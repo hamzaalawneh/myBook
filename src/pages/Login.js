17f7012ea2dde05687e1761b0f9cf24ded0e30e6
hamza alawneh <hamzaalawneh95@gmail.com>
2020-03-06 21:03:21 +0200

    fff

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,33 +15,13 @@ export default function Login() {
     }
     return (
 
-        // <Form>
-        //     <Form.Group controlId="formBasicEmail">
-        //         <Form.Label>Email address</Form.Label>
-        //         <Form.Control type="email" placeholder="Enter email" />
-        //         <Form.Text className="text-muted">
-        //             We'll never share your email with anyone else.
-        //   </Form.Text>
-        //     </Form.Group>
 
-        //     <Form.Group controlId="formBasicPassword">
-        //         <Form.Label>Password</Form.Label>
-        //         <Form.Control type="password" placeholder="Password" />
-        //     </Form.Group>
-        //     <Form.Group controlId="formBasicCheckbox">
-        //         <Form.Check type="checkbox" label="Check me out" />
-        //     </Form.Group>
-        //     <Button variant="primary" type="submit">
-        //         Submit
-        // </Button>
-        // </Form>
         <Container>
-
-            <Row>
-                <Col style={{backgroundColor:'red'}} xs lg="6">
-                    3 of 3
-            </Col>
-            </Row>
+            <div style={{ display: 'flex' }}>
+                <div style={{ backgroundColor: "red" }}>
+                    <p>fdfdfd</p>
+                </div>
+            </div>
         </Container>
 
     );
